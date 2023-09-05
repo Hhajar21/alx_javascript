05a8120e00f4c52c35668f346cd572550c4b7bf7
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-05 16:23:47 +0200

    103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,19 +4,19 @@
 
 You are not allowed to use var
  */
+
+
 const myObject = {
     type: 'object',
-    value: 12,
-    incr: function() {
-      this.value++;
-    }
+    value: 12
   };
   console.log(myObject);
+  myObject.incr = function () {
+    this.value++;
+  };
   myObject.incr();
   console.log(myObject);
   myObject.incr();
   console.log(myObject);
   myObject.incr();
   console.log(myObject);
-  
-  
