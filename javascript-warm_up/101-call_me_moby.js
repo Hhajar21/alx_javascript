9359bfb51d043b9ef88588876aa5fd6ce508740b
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-05 16:30:35 +0200

    101-call_me_moby.js

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -2,10 +2,8 @@
 /**
  * Write a function that executes x times a function.
 
-The function must be visible from outside
-Prototype: function (x, theFunction)
-You are not allowed to use var 
- */
+
+*/
 
 
 exports.callMeMoby = function(x, theFunction) {
