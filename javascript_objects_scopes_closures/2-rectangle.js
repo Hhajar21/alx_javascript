cf7f4ecd127347dfca554b6e91cae1fcc982db1e
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-05 16:58:02 +0200

    2-rectangle.js

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * Write a class Rectangle that defines a rectangle:
+ */
+
+
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // If w or h is not a positive integer, create an empty object
+        return {};
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
+
+  
