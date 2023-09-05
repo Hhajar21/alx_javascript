b010cab9e6fed44b082b2fc8d0a9b7996cc9e003
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-05 17:04:27 +0200

    3-rectangle.js

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Write a class Rectangle that defines a rectangle
+ */
+class Rectangle {
+    constructor(w, h) {
+        if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    }
+}
+module.exports = Rectangle;
