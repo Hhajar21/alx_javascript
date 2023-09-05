77db539fee02bd4ed01b63ab567481b067d649f6
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-05 16:27:27 +0200

    101-call_me_moby.js

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -6,9 +6,10 @@ The function must be visible from outside
 Prototype: function (x, theFunction)
 You are not allowed to use var 
  */
-exports.callMeMoby = function (x, theFunction) {
+
+
+exports.callMeMoby = function(x, theFunction) {
     for (let i = 0; i < x; i++) {
       theFunction();
     }
   };
-  
