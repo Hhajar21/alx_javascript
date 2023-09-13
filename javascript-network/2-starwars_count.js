53acb7fd4153e887e7622c88aa6a41baeeea7b7d
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-13 14:21:21 +0200

    2-starwars_count.js

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -5,32 +5,23 @@
  * This script prints the number of movies where the character "Wedge Antilles" is present.
  */
 
+
 const request = require('request');
-if (process.argv.length !== 3) {
-  console.error('Usage: node 2-starwars_count.js <API URL>');
-  process.exit(1);
-}
-const apiUrl = process.argv[2];
-const characterId = 18; 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+const API_URL = process.argv[2];
+let movieCount = 0;
 
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
-    process.exit(1);
-  }
-  try {
-    const filmData = JSON.parse(body);
-    const filmsWithWedgeAntilles = filmData.results.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-
-    console.log(filmsWithWedgeAntilles.length);
-  } catch (parseError) {
-    console.error(`Error parsing JSON: ${parseError.message}`);
-    process.exit(1);
-  }
+request(API_URL, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    }
+    const results = JSON.parse(body).results;
+    for (const filmIdx in results) {
+        const filmChars = results[filmIdx].characters;
+        for (const charIndex in filmChars) {
+            if (filmChars[charIndex].includes('18')) {
+                movieCount += 1;
+            }
+        }
+    }
+    console.log(movieCount);
 });
