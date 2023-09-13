8d41e046143f726242fdea98710ba7bf1ee1f0c1
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-13 14:29:54 +0200

    3-request_store.js

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -9,16 +9,12 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.log('Usage: node 3-request_store.js <URL> <file-path>');
-  process.exit(1);
-}
+
 
 request(url, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error}`);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: Received status code ${response.statusCode}`);
+  
   } else {
     fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
