530720440361227ce2121c8c8c48c38214c87c12
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-13 14:35:17 +0200

    3-request_store.js

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -4,24 +4,21 @@
  */
 
 
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
+
 const url = process.argv[2];
-const filePath = process.argv[3];
-
-
+const fileName = process.argv[3];
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(`Error writing to file: ${err}`);
-      } else {
-        console.log(`Contents of ${url} have been saved to ${filePath}`);
-      }
+    if (error) {
+        console.error(error);
+    }
+    const data = body;
+
+    fs.writeFile(fileName, data, 'utf-8', (err) => {
+        if (err) {
+            console.log(err);
+        }
     });
-  }
 });
