106de7a70978a81aef3779d398c24b74aceb1d79
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-13 15:39:21 +0200

    4-math.js

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -21,7 +21,7 @@ function createClassRoom(numberOfStudents) {
   
   var classRoom = createClassRoom(10);
   
-  console.log(classRoom[0]()); // Should return 1
-  console.log(classRoom[3]()); // Should return 4
-  console.log(classRoom[9]());  // Should return 10
+  console.log(classRoom[0]());  
+  console.log(classRoom[3]()); 
+  console.log(classRoom[9]());  
   
