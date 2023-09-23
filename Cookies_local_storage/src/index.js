17883fa0dc449efc6f0634d0d569974142abb07c
Hhajar21 <hajarhabibi016@gmail.com>
2023-09-23 14:11:58 +0200

    4-index.html

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,71 +1,56 @@
-function setCookies() {
-    var firstName = document.getElementById("firstname").value;
-    var email = document.getElementById("email").value;
+function setCookiesAndShowWelcomeMessage() {
+  var firstName = document.getElementById("firstname").value;
+  var email = document.getElementById("email").value;
   
-    var expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 10);
-    var formattedExpirationDate = expirationDate.toUTCString();
+  var expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 10);
 
-    document.cookie = "firstname=" + firstName + "; expires=" + formattedExpirationDate;
-    document.cookie = "email=" + email + "; expires=" + formattedExpirationDate;
+  // Use js-cookie to set cookies
+  Cookies.set("firstname", firstName, { expires: expirationDate });
+  Cookies.set("email", email, { expires: expirationDate });
 
-    window.location.href = "/";
-  
-   
-  }
-  function getCookie(name) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      if (cookie.indexOf(name + '=') === 0) {
-        return cookie.substring(name.length + 1);
-      }
-    }
-    return '';
-  }
+  showWelcomeMessageOrForm();
+}
 
-  function showCookies() {
-    var email = getCookie("email");
-    var firstname = getCookie("firstname");
+function showCookies() {
+  var email = Cookies.get("email");
+  var firstname = Cookies.get("firstname");
 
-    var paragraph = document.createElement("p");
-    paragraph.innerHTML = "Email: " + email + " - Firstname: " + firstname;
-    document.getElementById("cookieInfo").innerHTML = '';
-    document.getElementById("cookieInfo").appendChild(paragraph);
-  }
+  var paragraph = document.createElement("p");
+  paragraph.innerHTML = "Email: " + email + " - Firstname: " + firstname;
+  document.getElementById("cookieInfo").innerHTML = '';
+  document.getElementById("cookieInfo").appendChild(paragraph);
+}
 
-  function showForm() {
-    var welcomeMessage = document.getElementById("welcomeMessage");
-    var loginForm = document.getElementById("loginForm");
-    welcomeMessage.innerHTML = ''; 
-    loginForm.style.display = "block"; 
-  }
+function showForm() {
+  var welcomeMessage = document.getElementById("welcomeMessage");
+  var loginForm = document.getElementById("loginForm");
+  welcomeMessage.innerHTML = ''; 
+  loginForm.style.display = "block"; 
+}
 
+function hideForm() {
+  var loginForm = document.getElementById("loginForm");
+  loginForm.style.display = "none"; 
+}
 
-  function hideForm() {
-    var loginForm = document.getElementById("loginForm");
-    loginForm.style.display = "none"; 
-  }
+function deleteCookiesAndShowForm() {
+  // Use js-cookie to remove cookies
+  Cookies.remove("firstname");
+  Cookies.remove("email");
 
- 
-  function deleteCookiesAndShowForm() {
-    document.cookie = "firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-   
-  
-    showForm();
+  showForm();
 }
 
 function showWelcomeMessageOrForm() {
-    var firstname = getCookie("firstname");
+  var firstname = Cookies.get("firstname");
 
-    if (firstname) {
-        var welcomeMessage = document.getElementById("welcomeMessage");
-        welcomeMessage.innerHTML = 'Welcome ' + firstname + ' <a href="#" onclick="deleteCookiesAndShowForm();"> (logout)</a>';
-    } else {
-        
-        showForm();
-    }
+  if (firstname) {
+    var welcomeMessage = document.getElementById("welcomeMessage");
+    welcomeMessage.innerHTML = 'Welcome ' + firstname + ' <a href="#" onclick="deleteCookiesAndShowForm();"> (logout)</a>';
+  } else {
+    showForm();
+  }
 }
 
 showWelcomeMessageOrForm();
